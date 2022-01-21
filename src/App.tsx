b19b489b2255e8064133e9ddb0edfe3a16aa4a09
Mohd Adul Khadir Naveed <59957376+mdnaveed64@users.noreply.github.com>
2022-01-21 13:21:18 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,65 @@
+import React, { FC, ChangeEvent, useState } from "react";
+import "./App.css";
+import TodoList from "./Components/TodoList";
+import { ITask } from "./InterFaces";
+
+const App: FC = () => {
+    const [task, setTask] = useState<string>("");
+    const [deadline, setDealine] = useState<number>(0);
+    const [todoList, setTodoList] = useState<ITask[]>([]);
+
+    const handleChange = (event: ChangeEvent<HTMLInputElement>): void => {
+        if (event.target.name === "task") {
+            setTask(event.target.value);
+        } else {
+            setDealine(Number(event.target.value));
+        }
+    };
+
+    const addTask = (): void => {
+        const newTask = { taskName: task, deadline: deadline };
+        setTodoList([...todoList, newTask]);
+        setTask("");
+        setDealine(0);
+    };
+
+    const completeTask = (taskNameToDelete: string): void => {
+        setTodoList(
+            todoList.filter((task) => {
+                return task.taskName != taskNameToDelete;
+            })
+        );
+    };
+
+    return (
+        <div className="App">
+            <div className="header">
+                <div className="inputContainer">
+
+                    <input
+                        type="text"
+                        placeholder="Task..."
+                        name="task"
+                        value={task}
+                        onChange={handleChange}
+                    />
+                    <input
+                        type="number"
+                        placeholder="Deadline (in Days)..."
+                        name="deadline"
+                        value={deadline}
+                        onChange={handleChange}
+                    />
+                </div>
+                <button className="btn btn-danger" onClick={addTask}>Add Task</button>
+            </div>
+            <div className="todoList">
+                {todoList.map((task: ITask, key: number) => {
+                    return <TodoList key={key} task={task} completeTask={completeTask} />;
+                })}
+            </div>
+        </div>
+    );
+};
+
+export default App;
